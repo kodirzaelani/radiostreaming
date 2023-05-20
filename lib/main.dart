50c22c72460aeb5ed4d912fa7fb3f9885196ca6f
kodirzaelani <laman.kreasi@gmail.com>
2023-05-20 16:38:13 +0800

    Commit Pertama

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:radiosdk26/pages/radio_stream.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Streaming Radio',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const StreamRadioPage(),
+    );
+  }
+}
