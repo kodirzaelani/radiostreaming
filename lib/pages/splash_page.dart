245690e8fb3c0e806a5568d7ee5d10c5dbc1216d
kodirzaelani <laman.kreasi@gmail.com>
2023-05-20 17:08:20 +0800

    add them, google fonts

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import '../theme.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  startSplashScreen() async {
+    var duration = const Duration(seconds: 5);
+    return Timer(duration, () {
+      Navigator.pushNamed(context, '/sign-in');
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startSplashScreen();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor1,
+      body: Center(
+        child: Container(
+          width: 130,
+          height: 150,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                'assets/cover.png',
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
